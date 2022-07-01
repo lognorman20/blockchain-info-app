31f3d41f2f88a7d5f8db6083e99ba283ad358435
Logan Norman <logannormanss@gmail.com>
2022-07-01 16:43:02 -0700

    init send_request

---
+++ src/blockchain_info.rs
@@ -0,0 +1,27 @@
+use dotenv;
+use reqwest;
+use tokio;
+use serde_json::Result;
+use crate::blockchain_status::BlockchainStatus;
+use crate::blockchain_address::BlockchainAddress;
+use crate::blockchain_tx::BlockchainTx;
+
+const ROOT_URL: &str = "https://btcbook.nownodes.io/api/v2/";
+
+#[tokio::main]
+pub async fn send_request(url: &str) -> String {
+    let client = reqwest::Client::new();
+    client
+        .get(url)
+        .header("api-key", dotenv::var("API_KEY").expect("Could not find API key :("))
+        .send()
+        .await
+        .expect("Failed to get data from API :( ")
+        .text()
+        .await
+        .expect("Failed to convert to text :(")
+}
+
+pub fn blockchain_status_request() {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#[macro_use]
+extern crate serde_derive; 
+
+mod blockchain_status;
+mod blockchain_info;
+mod blockchain_tx;
+mod blockchain_address;
+
 fn main() {
     println!("Hello, world!");
 }
